a4a34c2ff7cd84a6c0c611e81279b60657d2c5c1
tayv <48400779+tayv@users.noreply.github.com>
2024-08-05 21:55:48 -0600

    add main.js and remove header-loading.js

--- src/js/load-header.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-        });
-});
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header.js"
+
+document.addEventListener("DOMContentLoaded", function () {
+  // event listener used so that Header() runs after the DOM has loaded
+  const headerElement = document.querySelector("header")
+  const headerContent = Header() // Check if Header() (Header.js) returns the expected content
+  // console.log(headerContent) // Log the header content to verify it works
+  headerElement.appendChild(headerContent) // Append the header content to the header element in index.html
+})
