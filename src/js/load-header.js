2534098e310f9e88819ae77290e8f2932bc9d784
Kim vanderHelm <kvanderhelm@me.com>
2024-08-08 15:02:29 -0600

    bug: font loading error fix

--- src/js/load-header.js
+++ src/js/load-header.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
